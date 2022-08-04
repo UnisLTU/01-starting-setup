4fd86bd62f9a3b15a3dabd66c9a885f0ac21ad87
UnisLTU <109076072+UnisLTU@users.noreply.github.com>
2022-08-04 21:16:25 +0300

    Start of State

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,9 +1,10 @@
+import React from "react";
 import './ExpenseDate.css'
 
 const ExpenseDate = (props) => {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
+  const month = props.dateTime.toLocaleString("en-US", { month: "long" });
+  const day = props.dateTime.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.dateTime.getFullYear();
 
   return (
     <div className='expense-date'>
---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "../Expenses/ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+
+const ExpenseItem = (props) => {
+
+  let title = props.title;
+
+  const clickHandler = () => {
+    title = "Updated!"
+  };
+
+  return (
+    <Card className="expense-item">
+        <ExpenseDate dateTime={props.date}/>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change title</button>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
